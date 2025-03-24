bfb659a4911e28fba1761f6682892ba94da1e231
Titov Ilya <titov1608@gmail.com>
2025-03-24 16:38:33 +0300

    hm-12-2

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,22 @@
-def get_mask_card_number(number_card: int) -> str:
-    """Функцию маскировки номера банковской карты """
-    str_number_card = str(number_card)
+import logging
+
+def get_mask_card_number(number_card: str) -> str:
+    """Функцию маскировки номера банковской карты"""
+
+    logger = logging.getLogger("masks")
+    logger.setLevel(logging.DEBUG)
+
+    formater = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    file_handler = logging.FileHandler("logs/masks.log", mode='a', encoding="utf-8")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formater)
+
+    logger.addHandler(file_handler)
+
+    str_number_card = number_card
     masked = str_number_card[:6] + "*" * 6 + str_number_card[-4:]
 
     count = 0
@@ -10,11 +26,12 @@ def get_mask_card_number(number_card: int) -> str:
         slice_masc += i
         if count % 4 == 0:
             slice_masc += " "
-    return slice_masc
+
+    return slice_masc[0:19]
 
 
-def get_mask_account(number_acc: int) -> str:
-    """Функцию маскировки номера банковского счета """
-    str_number_acc = str(number_acc)
+def get_mask_account(number_acc: str) -> str:
+    """Функцию маскировки номера банковского счета"""
+    str_number_acc = number_acc
     masked = "**" + str_number_acc[-4:]
     return masked
--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,40 @@
+import logging
 import json
 import os
 
 
 def transactions(json_file):
+    logger = logging.getLogger("transaction")
+    logger.setLevel(logging.DEBUG)
+
+    formater = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    file_handler = logging.FileHandler("logs/utils.log", mode='a', encoding="utf-8")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formater)
+
+    logger.addHandler(file_handler)
 
     if not os.path.isfile(json_file):
+        logger.warning("Файл не найден: %s")
         return []
     if os.path.getsize(json_file) == 0:
+        logger.warning("Файл пуст: %s")
         return []
 
     try:
+        logger.debug("Запущенна функция открытия и чтения json.file: %s")
         with open(json_file, encoding="utf-8") as f:
             data = json.load(f)
             if isinstance(data, list):
+                logger.info("Функция успешно сработала")
                 return data
             else:
+                logger.warning("Функция успешно сработала, но в файле не найден LIST")
                 return []
 
     except (json.JSONDecodeError, ValueError):
-        print("Ошибка чтения файла или неверный формат данных.")
+        logger.error("Ошибка чтения файла или неверный формат данных.")
         return []
