ca8008f5683983e82840a659c188da952a9412ba
Titov Ilya <titov1608@gmail.com>
2025-03-27 09:05:14 +0300

    fixed hm_13.1

--- src/read_transaction.py
+++ src/read_transaction.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 
 def reading_csv(csv_file):
+    """Функция чтения csv файлов"""
     transactions_list = []
     with open(csv_file, encoding="utf-8") as file:
         reader = csv.DictReader(file)
@@ -11,6 +12,7 @@ def reading_csv(csv_file):
 
 
 def reading_xlsx(xlsx_file):
+    """Функция чтения xlsx файлов"""
     exel_data = pd.read_excel(xlsx_file)
     transactions_list = exel_data.to_dict(orient="records")
 
